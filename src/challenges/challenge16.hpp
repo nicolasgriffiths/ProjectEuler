580620c25588d2730bc238b08e60eb3f2be15fc1
Nicolas Griffithsclear <nicool80@hotmail.com>
2019-04-27 12:36:14 +0100

    typo

--- src/challenges/challenge16.hpp
+++ src/challenges/challenge16.hpp
@@ -5,8 +5,8 @@
 #include "challenge_interface.hpp"
 
 /*
- * 215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
- * What is the sum of the digits of the number 21000?
+ * 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+ * What is the sum of the digits of the number 2^1000?
  */
 
 namespace euler {
